02a79ccd8e92609a98a92a97b3ca34cd772d2707
chuelmo-utec <christian.huelmo@estudiantes.utec.edu.uy>
2021-01-07 01:10:44 -0300

    Segunda clase: let, const, var, strings multilínea, spread operator u operador de desagregación y desestructuración

--- src/es6/index.js
+++ src/es6/index.js
@@ -25,3 +25,47 @@ console.log(epicPhrase)
 let epicPhrase2 = `${hello} ${world}`
 console.log(epicPhrase2)
 
+//before
+let lorem = 'Lorem ipsum dolor sit amet consectetur adipiscing elit\n'
+    + 'justo sodales ultrices, vehicula accumsan facilisi sapien\n'
+    + 'curabitur per blandit litora.\n'
+    + 'Erat eget litora dis nisl hac accumsan himenaeos et pellentesque\n'
+    + 'fermentum inceptos montes ad massa, dictumst morbi rhoncus\n'
+    + 'etiam vestibulum mollis class mus laoreet consequat tortor ac dictum.\n'
+    + 'Parturient gravida orci placerat cras montes'
+console.log(lorem)
+
+//ES6
+let lorem2 = `Lorem ipsum dolor sit amet consectetur adipiscing elit
+justo sodales ultrices, vehicula accumsan facilisi sapien
+curabitur per blandit litora.
+Erat eget litora dis nisl hac accumsan himenaeos et pellentesque
+fermentum inceptos montes ad massa, dictumst morbi rhoncus
+etiam vestibulum mollis class mus laoreet consequat tortor ac dictum.
+Parturient gravida orci placerat cras montes`
+console.log(lorem2)
+
+//desestructuración
+let person = {
+    name: 'oscar',
+    age: 32,
+    country: 'MX'
+}
+
+console.log(person.name, person.age, person.country)
+let {name, age, country} = person
+console.log(name, age, country)
+
+//operador de propagación
+let team1 = ['Oscar', 'Julian', 'Ricardo']
+let team2 = ['Valeria', 'Yesica', 'Camila']
+let education = ['David', ...team1, ...team2]
+
+console.log(education)
+
+//var, let, const
+{var globalVar = "Variable Global"}
+{let globalLet = 'Variable Global Let'}
+
+console.log(globalVar)
+console.log(globalLet) //<-- ERROR!!!
