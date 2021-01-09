786a5b6e71860cdddbb35bb48701b8d873030819
chuelmo-utec <christian.huelmo@estudiantes.utec.edu.uy>
2021-01-08 22:27:22 -0300

    Clase 4: Arrow Functions, Promesas y parámetros en objetos

--- src/es6/index.js
+++ src/es6/index.js
@@ -69,3 +69,76 @@ console.log(education)
 
 console.log(globalVar)
 console.log(globalLet) //<-- ERROR!!!
+
+let name = 'chuelmo'
+let age = 48
+
+//es5
+objeto = {name: name, age: age}
+console.log(objeto)
+
+//es6
+objeto2 = {name, age}
+console.log(objeto2)
+
+// arrow functions
+const names = [
+    {name: 'Christian', age: 48},
+    {name: 'Yesica', age:27}
+]
+
+let listOfNames = names.map(function(item){
+    console.log(`${item.name} ${item.age}`)
+})
+
+let listOfNames2 = names.map(item => console.log(`${item.name} ${item.age}`))
+
+const listOfNames3 = ({name, age}) => {
+    console.log(`${name} ${age}`)
+}
+
+listOfNames3(names[0])
+
+const listOfNames4 = ({name}) => {
+    console.log(`${name}`)
+}
+
+listOfNames4(names[1])
+
+const square = num => num * num
+
+console.log(square(5))
+
+// Una última prueba
+const nombre = 'Jorge'
+const edad = 43
+const pais = 'Uruguay'
+
+const persona = {
+    nombre: 'Juan',
+    edad: 45,
+    pais: 'Uruguay'
+}
+
+const saludar = ({nombre, edad, pais}) => {
+    console.log(`Hola, soy ${nombre}, tengo ${edad} años y soy de ${pais}`)
+}
+
+saludar(persona)
+saludar(nombre, edad, pais) // <-- esto no anda jejejej, pero tenía que probar
+
+const helloPromise = () => {
+    return new Promise((resolve, reject) => {
+        if (false) {
+            resolve('Todo OK!')
+        } else {
+            reject('Ups! Algo no anduvo bien')
+        }
+    })
+}
+
+helloPromise()
+    .then(response => console.log(response))
+    .then(() => console.log('Hola, todo muy bien por aquí'))
+    .catch(error => console.log(error))
+
