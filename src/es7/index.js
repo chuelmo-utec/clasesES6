eed473fdb33d85af3efa48b4ca420c351a6acef9
chuelmo-utec <christian.huelmo@estudiantes.utec.edu.uy>
2021-01-08 23:42:07 -0300

    Cambios en ES7

---
+++ src/es7/index.js
@@ -0,0 +1,16 @@
+let numbers = [1,2,3,7,8]
+
+const valorBuscado = 1
+const desdeLaPosicion = 0
+
+if (numbers.includes(valorBuscado, desdeLaPosicion)) {
+    console.log(`Se encontró el valor ${valorBuscado}`)
+} else {
+    console.log(`No se encuentra el valor ${valorBuscado} a partir de la posición ${desdeLaPosicion}`)
+}
+
+let base = 4
+let exponente = 3
+let resultado = base ** exponente
+console.log(resultado)
+
